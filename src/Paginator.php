15e2a9f99eb71a8b58adeb4e1147d61d9b1efafc
csbragin <csbragin@gmail.com>
2023-05-21 03:45:18 +0300

    fix: flowbite & tailwind support

--- src/Paginator.php
+++ src/Paginator.php
@@ -13,12 +13,8 @@ class Paginator
     protected int $currentPage;
     protected string $urlPattern;
     protected int $maxPagesToShow = 10;
-    protected string $previousText = '<span class="sr-only">Предыдущая</span><svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-                                        </svg>';
-    protected string $nextText = '<span class="sr-only">Следующая</span><svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                                        </svg>';
+    protected string $previousText = 'Предыдущая';
+    protected string $nextText = 'Следующая';
 
     /**
      * @param int $totalItems the total number of items
@@ -28,6 +24,8 @@ class Paginator
      */
     public function __construct(int $totalItems, int $itemsPerPage, int $currentPage, string $urlPattern = '')
     {
+        $this->previousText = sprintf('<span class="sr-only">%s</span><svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>', __($this->previousText));
+        $this->nextText = sprintf('<span class="sr-only">%s</span><svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>', __($this->nextText));
         $this->totalItems = $totalItems;
         $this->itemsPerPage = $itemsPerPage;
         $this->currentPage = $currentPage;
